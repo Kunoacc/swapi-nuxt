69084c63964a2bd05b9c201578de17059fd70b8c
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2019-08-13 01:18:07 +0100

    Feat: Added content loaders

---
+++ components/loaders/PlanetLoader.vue
@@ -0,0 +1,22 @@
+
+
+<script>
+  import { ContentLoader } from "vue-content-loader"
+
+  export default {
+    components: {
+      ContentLoader
+    }
+  }
+</script>
+<template>
+  <content-loader
+    :height="475"
+    :width="600"
+    :speed="2"
+    primaryColor="#f5f5f5"
+    secondaryColor="#ecebeb"
+  >
+    <rect x="12" y="68" rx="5" ry="5" width="292" height="272" />
+  </content-loader>
+</template>
---
+++ components/loaders/UserLoader.vue
@@ -0,0 +1,25 @@
+<script>
+  import { ContentLoader } from "vue-content-loader"
+
+  export default {
+    components: {
+      ContentLoader
+    }
+  }
+</script>
+<template>
+  <content-loader
+    :height="475"
+    :width="600"
+    :speed="2"
+    primaryColor="#f5f5f5"
+    secondaryColor="#ecebeb"
+  >
+    <rect x="13" y="65" rx="5" ry="5" width="184" height="228" />
+    <rect x="216" y="62" rx="0" ry="0" width="168" height="38" />
+    <rect x="218" y="139" rx="0" ry="0" width="168" height="13.2" />
+    <rect x="220" y="161" rx="0" ry="0" width="168" height="13.2" />
+    <rect x="219" y="183" rx="0" ry="0" width="168" height="13.2" />
+    <rect x="220" y="205" rx="0" ry="0" width="168" height="13.2" />
+  </content-loader>
+</template>
