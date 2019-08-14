519e11fdb34cc58aefda0dae4ce40bcb76d30535
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2019-08-15 00:10:17 +0100

    Feat: setup vuex

---
+++ store/index.js
@@ -0,0 +1,31 @@
+import Vuex from "vuex"
+
+const state = () => {
+
+}
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+const getters = {
+
+}
+
+const createStore = () => {
+    return new Vuex.Store({
+        modules: {
+
+        },
+        state,
+        mutations,
+        actions,
+        getters
+    })
+}
+
+export default createStore
