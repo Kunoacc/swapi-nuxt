bcd0850366122df410390f260edd854b83c6a370
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2019-08-15 00:09:41 +0100

    Feature: Created global components registry

--- plugins/global-components.js
+++ plugins/global-components.js
@@ -1,11 +1,11 @@
-import Vue from "vue"
-import _ from "lodash.foreach";
+import Vue from 'vue'
+import _ from 'lodash.foreach'
 
 const components = require.context('~/components', false, /[A-Z]\w+\.(vue)$/)
 const keys = components.keys()
-_(keys, fileName => {
-    const componentConfig  = components(fileName)
-    console.log(`component - ${fileName}`)
-    const componentName = fileName.split('/').pop().split('.')[0]
-    Vue.component(componentName, componentConfig.default || componentConfig)
+_(keys, (fileName) => {
+  const componentConfig = components(fileName)
+  console.log(`component - ${fileName}`)
+  const componentName = fileName.split('/').pop().split('.')[0]
+  Vue.component(componentName, componentConfig.default || componentConfig)
 })
--- plugins/global-loaders.js
+++ plugins/global-loaders.js
@@ -1,10 +1,10 @@
-import Vue from "vue"
-import _ from "lodash.foreach"
+import Vue from 'vue'
+import _ from 'lodash.foreach'
 
 const components = require.context('~/components/loaders', false, /[A-Z]\w+\.(vue)$/)
 const keys = components.keys()
-_(keys, fileName => {
-    const componentConfig  = components(fileName)
-    const componentName = fileName.split('/').pop().split('.')[0]
-    Vue.component(componentName, componentConfig.default || componentConfig)
+_(keys, (fileName) => {
+  const componentConfig = components(fileName)
+  const componentName = fileName.split('/').pop().split('.')[0]
+  Vue.component(componentName, componentConfig.default || componentConfig)
 })
