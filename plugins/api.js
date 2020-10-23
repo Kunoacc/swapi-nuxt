e8f2527478a3c43adcc41cb91dccd670637c432d
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2020-10-23 21:29:21 +0100

    Update api.js
    updated broken API

--- plugins/api.js
+++ plugins/api.js
@@ -3,7 +3,7 @@ import { api as httpApi } from '../api'
 export default function ({
   config
 }, inject) {
-  const getData = url => fetch(`https://swapi.co/api/${url}`, {
+  const getData = url => fetch(`https://swapi.dev/api/${url}`, {
     credentials: 'omit',
     method: 'GET',
     // mode: 'no-cors',
